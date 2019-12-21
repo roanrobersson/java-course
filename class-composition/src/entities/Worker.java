cafdd35058110d0b78dc197520b2930b1421ef18
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2019-12-21 02:00:33 -0300

    Section 13 - Add entities and enums

---
+++ class-composition/src/entities/Worker.java
@@ -0,0 +1,87 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.WorkerLevel;
+
+public class Worker {
+
+	private String name;
+	private WorkerLevel level;
+	private double baseSalary;
+	private Department department;
+	private List<HourContract> contracts = new ArrayList<>();;
+	
+	public Worker() {
+	}
+
+	public Worker(String name, WorkerLevel level, double baseSalary, Department department) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+	
+	public void addContract(HourContract contract) {
+		this.contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		this.contracts.remove(contract);
+	}
+	
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		
+		for(HourContract c : this.contracts) {
+			cal.setTime(c.getDate());
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			int c_year = cal.get(Calendar.YEAR);
+			if (month == c_month && year == c_year) {
+				sum += c.totalValue();
+			}
+		}
+		
+		return sum;
+	}
+	
+}
