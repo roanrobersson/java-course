a2df50f4f72e1d43dfac1002d41d602737e6d59e
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2021-04-24 17:53:10 -0300

    Section 24 - JPA-Hibernate

---
+++ 24/src/main/java/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import domain.Person;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Person p1 = new Person(null, "Carlos da Silva", "carlos@gmail.com");
+		Person p2 = new Person(null, "Joaquim Torres", "joaquim@gmail.com");
+		Person p3 = new Person(null, "Ana Maria", "ana@gmail.com");	
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Database populated!");
+		
+		/*
+		Person p1 = em.find(Person.class, 1);
+		System.out.println(p1);
+		*/
+
+		em.close();
+		emf.close();
+	}
+
+}
